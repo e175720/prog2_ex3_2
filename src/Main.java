9b0cc130096e8a73b39baf14b6405ba6e86acccd
Matsuda Masami <e175720@ie.u-ryukyu.ac.jp>
2017-12-05 10:40:27 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import jp.ac.uryukyu.ie.e175720.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Dice dice = new Dice();
+        System.out.println(dice.getValue());
+        ExDice exdice = new ExDice(5, 7);
+
+
+    }
+
+}
